8e2fb84208b45cd2cd723d722c2188dee4964d5b
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-05-01 14:52:16 -0300

    add !info command

--- bot.py
+++ bot.py
@@ -204,15 +204,44 @@ async def draw_card(ctx) :
     mood = random.choice(moods)
     # Format the response message
     response = (
-    f'```**Here\'s your ATOM draw!**\n'
-    f'**A - Arc Card Draw:**\nArc Type: {arc_type}\nArc Timeline: {arc_timeline}\n'
-    f'**T - Terrain Card Draw:**\nTerrain: {terrain}\n'
-    f'**O - Object Card Draw:**\nObject: {object}\n'
-    f'**M - Mood Card Draw:**\nMood: {mood}```'
+    f'\n**Here\'s your ATOM draw!**\n\n'
+    f'**A - Arc Card Draw:**\n> Arc Type: {arc_type}\n> Arc Timeline: {arc_timeline}\n\n'
+    f'**T - Terrain Card Draw:**\n> Terrain: {terrain}\n\n'
+    f'**O - Object Card Draw:**\n> Object: {object}\n\n'
+    f'**M - Mood Card Draw:**\n> Mood: {mood}'
     )
     # Send the response message to the Discord channel
     await ctx.send (response)
     
+@bot.command(name='info')
+async def info(ctx) :
+    help_message = (
+    '''
+:globe_with_meridians:  **The Thing From The Future / ATOM is an imagination game.** \nThe object of the game is to use the cards to generate the most interesting, funny or thought-provoking ideas for artifacts from the future. There are over 3.7 million possible prompts in the deck. 
+
+:black_joker: **Use _!draw_ to shuffle the deck and get a unique set of cards.**
+
+
+**There are 4 cards deployed in every shuffle** :small_blue_diamond: :small_blue_diamond: :small_blue_diamond: :small_blue_diamond: 
+
+> **A - ARC** outlines the type of future world that the “thing” comes from, and how far away it is from today. _There are four types of Arc, each an umbrella for countless possible scenarios:_
+> _------- Growth - a future in which “progress” has continued_
+> _------- Collapse – a future in which society as we know it has come apart_
+> _------- Discipline - a future in which order is deliberately coordinated or imposed_
+> _------- Transformation – a future in which a profound historical evolution has occurred_
+> 
+> **T - TERRAIN** is the thematic context or location where this object could be found in that future.
+> 
+> **O - OBJECT** is the focus for your imagination: a specific cultural artifact that reveals something about how this future is different from today.
+> 
+> **M - MOOD** suggests how it might feel to experience this thing from the future.
+
+
+*A game by Stuart Candy and Jeff Watson*
+2015 CC-BY-NC-SA | situationlab.org | @sitlab
+Discord implementation by JUNK Consortium - Universidad Austral, Argentina (2024)
+    ''')
+    await ctx.send(help_message)
     
 # Start the bot with your unique token
 bot.run(os.environ["BOT_TOKEN"])
